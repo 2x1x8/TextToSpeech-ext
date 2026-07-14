94b51770d243b4318ec423e758b8087b8b0ced37
2x1x8 <quocanh.thesheep@gmail.com>
2026-07-14 13:32:53 +0700

    .

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse)=>{
-    console.log(message)
-		const audio = new Audio(
-			`data:audio/mp3;base64,${message.audio}`
-		);
-		audio.play()
+  console.log(message)
+	const audio = new Audio(
+		`data:audio/mp3;base64,${message.audio}`
+	);
+	audio.play()
 })
